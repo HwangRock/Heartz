20d1b1b3106f8ddfabaadea23e19103ccfd67311
Roflaff <kimsp0317@gmail.com>
2025-02-12 20:34:32 +0900

    workflow.yml

--- src/test/java/blaybus/mvp/back/BackApplicationTests.java
+++ src/test/java/blaybus/mvp/back/BackApplicationTests.java
@@ -2,12 +2,19 @@ package blaybus.mvp.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class BackApplicationTests {
 
+	@Autowired
+	private Environment environment;
+
 	@Test
-	void contextLoads() {
+	void checkDatabaseConfig() {
+		System.out.println("DB URL: " + environment.getProperty("spring.datasource.url"));
+		System.out.println("DB Username: " + environment.getProperty("spring.datasource.username"));
 	}
 
 }
