93cfd2e5cb0c866df62a4c9176ba6211fa7f08e5
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-02-17 01:53:00 +0900

    refactor: config, 토큰 발행 수정

--- src/main/java/blaybus/mvp/back/config/SecurityConfig.java
+++ src/main/java/blaybus/mvp/back/config/SecurityConfig.java
@@ -1,109 +1,34 @@
 package blaybus.mvp.back.config;
 
-import blaybus.mvp.back.filter.TokenAuthenticationFilter;
-import blaybus.mvp.back.filter.TokenExceptionFilter;
-import blaybus.mvp.back.handler.CustomAccessDeniedHandler;
-import blaybus.mvp.back.handler.CustomAuthenticationEntryPoint;
-import blaybus.mvp.back.handler.OAuth2FailureHandler;
-import blaybus.mvp.back.handler.OAuth2SuccessHandler;
-import blaybus.mvp.back.service.CustomOAuth2UserService;
-import lombok.RequiredArgsConstructor;
+import blaybus.mvp.back.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final TokenAuthenticationFilter tokenAuthenticationFilter;
+    private final TokenProvider jwtTokenProvider;
 
-    @Bean
-    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-        return new MvcRequestMatcher.Builder(introspector);
+    public SecurityConfig(TokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-
-        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
-
-        // white list (Spring Security 체크 제외 목록)
-        MvcRequestMatcher[] permitAllWhiteList = {
-                mvc.pattern("/api/vi/user/login-test"),
-                mvc.pattern("/login/oauth2/code/google"),
-                //mvc.pattern("/token-refresh"),
-                mvc.pattern("/favicon.ico"),
-                mvc.pattern("/error")
-        };
-
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(
-                        (csrfConfig) -> csrfConfig.disable()
-                )
-        //Form 로그인 방식 disable
-                .formLogin((auth) -> auth.disable())
-        //http basic 인증 방식 disable
-                .httpBasic((auth) -> auth.disable())
-                .headers(
-                        (headerConfig) -> headerConfig.frameOptions(
-                                frameOptionsConfig -> frameOptionsConfig.disable()
-                        )
-                )
-                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
-                        //.requestMatchers(permitAllWhiteList).permitAll()
-                        .requestMatchers("/test", "/oauth2/authorization/google", "/login/oauth2/code/google", "/error", "/favicon.ico").permitAll()
+                .csrf(csrf -> csrf.disable()) // 새롭게 변경된 CSRF 비활성화 방식
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/oauth2/authorization/google", "/login/oauth2/code/google", "/error", "/favicon.ico").permitAll()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(c ->
-                        c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                //이 부분 수정ㅈ~~~~~~~~~~~~~~~~~~~~~
-                .logout( // 로그아웃 성공 시 / 주소로 이동
-                        (logoutConfig) -> logoutConfig.logoutSuccessUrl("/")
-                )
-                // OAuth2 로그인 기능에 대한 여러 설정
-                .oauth2Login(oauth -> // OAuth2 로그인 기능에 대한 여러 설정의 진입점
-                        // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정을 담당
-                        oauth.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                                // 로그인 성공 시 핸들러
-                                .successHandler(oAuth2SuccessHandler)
-                                // 로그인 실패 시 핸들러
-                                .failureHandler(new OAuth2FailureHandler())
-                                //로그인 실패 시 리다이렉트할 로그인 페이지
-                                //.loginPage("/api/vi/user/login-test")
-                )
-                // jwt 관련 설정
-                .addFilterBefore(tokenAuthenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new TokenExceptionFilter(), tokenAuthenticationFilter.getClass()) // 토큰 예외 핸들링
-
-                // 인증 예외 핸들링
-                .exceptionHandling((exceptions) -> exceptions
-                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-                        .accessDeniedHandler(new CustomAccessDeniedHandler()));
-
-        /*
-                .oauth2Login(
-                        (oauth) ->
-                            oauth.userInfoEndpoint(
-                                    (endpoint) -> endpoint.userService(customOAuth2UserService)
-                            )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/login-success", true)
                 );
-        */
 
         return http.build();
     }
-
 }
--- src/main/java/blaybus/mvp/back/jwt/TokenProvider.java
+++ src/main/java/blaybus/mvp/back/jwt/TokenProvider.java
@@ -1,195 +1,67 @@
 package blaybus.mvp.back.jwt;
 
-import blaybus.mvp.back.domain.Client;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SecurityException;
 import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.util.Collections;
+import java.security.Key;
 import java.util.Date;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-@Slf4j
-@RequiredArgsConstructor
 @Component
 public class TokenProvider {
 
+    private Key secretKey;
+
     @Value("${jwt.secret}")
     private String key;
-    private SecretKey secretKey;
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L;
-    //private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60L * 24 * 7;
-    private static final String KEY_ROLE = "role";
-    //private final TokenService tokenService;
+
+    private final long validityInMilliseconds = 60 * 60 * 1000; // 1시간
 
     @PostConstruct
-    private void init() {
-        secretKey = Keys.hmacShaKeyFor(key.getBytes());
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(key.getBytes()); // 설정 파일에서 주입된 비밀키 사용
     }
 
-    public String generateAccessToken(Authentication authentication) {
-        return generateToken(authentication, ACCESS_TOKEN_EXPIRE_TIME);
-    }
-
-//    // 1. refresh token 발급
-//    public void generateRefreshToken(Authentication authentication, String accessToken) {
-//        String refreshToken = generateToken(authentication, REFRESH_TOKEN_EXPIRE_TIME);
-//        tokenService.saveOrUpdate(authentication.getName(), refreshToken, accessToken); // redis에 저장
-//    }
-
-    private String generateToken(Authentication authentication, long expireTime) {
+    public String createToken(String username, String email) {
         Date now = new Date();
-        Date expiredDate = new Date(now.getTime() + expireTime);
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
 
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining());
-
-        log.info("authentication.getName(): {}", authentication.getName());
-        log.info("authorities: {}", authorities);
         return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(KEY_ROLE, authorities)
+                .setSubject(username)
+                .claim("email", email)
                 .setIssuedAt(now)
-                .setExpiration(expiredDate)
+                .setExpiration(validity)
                 .signWith(secretKey)
                 .compact();
     }
 
-    //해당 토큰의 authentication 정보 가져오기 위해 사용
-    public Authentication getAuthentication(String token) {
-        Claims claims = getAllClaimsFromToken(token);
-        List<SimpleGrantedAuthority> authorities = getAuthorities(claims);
-
-        User principal = new User(claims.getSubject(), "", authorities);
-        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    private List<SimpleGrantedAuthority> getAuthorities(Claims claims) {
-        return Collections.singletonList(new SimpleGrantedAuthority(
-                claims.get(KEY_ROLE).toString()));
-    }
-
-//    // 3. accessToken 재발급
-//    public String reissueAccessToken(String accessToken) {
-//        if (StringUtils.hasText(accessToken)) {
-//            Token token = tokenService.findByAccessTokenOrThrow(accessToken);
-//            String refreshToken = token.getRefreshToken();
-//
-//            if (validateToken(refreshToken)) {
-//                String reissueAccessToken = generateAccessToken(getAuthentication(refreshToken));
-//                tokenService.updateToken(reissueAccessToken, token);
-//                return reissueAccessToken;
-//            }
-//        }
-//        return null;
-//    }
-
-
-//    private Claims parseClaims(String token) {
-//        try {
-//            return Jwts.parser().verifyWith(secretKey).build()
-//                    .parseSignedClaims(token).getPayload();
-//        } catch (ExpiredJwtException e) {
-//            return e.getClaims();
-//        } catch (MalformedJwtException e) {
-//            throw new TokenException(INVALID_TOKEN);
-//        } catch (SecurityException e) {
-//            throw new TokenException(INVALID_JWT_SIGNATURE);
-//        }
-//    }
-
-    public String getUserIdFromToken(final String token){
-        String userId = getClaimFromToken(token, Claims::getSubject);
-        return userId;
-    }
-
-    public String getRoleFromToken(final String token){
-        String role =getClaimFromToken(token, claims -> claims.get("role", String.class));
-        return role;
-    }
-
-    public Boolean isExpiredToken(final String token){
-        Boolean isExpired = getClaimFromToken(token, Claims::getExpiration).before(new Date());
-        return isExpired;
-    }
-
-//    public String getRole(final String token){
-//        String role = String.valueOf(getAllClaimsFromToken(token).get("role"));
-//        log.info("getUsernameFormToken subject = {}", role);
-//        return role;
-//    }
-
-    /**
-     * token 사용자 속성 정보 조회
-     *
-     * @param token JWT
-     * @param claimsResolver Get Function With Target Claim
-     * @param <T> Target Claim
-     * @return 사용자 속성 정보
-     */
-    public <T> T getClaimFromToken(final String token, final Function<Claims, T> claimsResolver) {
-        // token 유효성 검증
-        if(Boolean.FALSE.equals(validateToken(token)))
-            return null;
-
-        final Claims claims = getAllClaimsFromToken(token);
-
-        return claimsResolver.apply(claims);
-    }
-
-    /**
-     * token 사용자 모든 속성 정보 조회
-     *
-     * @param token JWT
-     * @return All Claims
-     */
-    private Claims getAllClaimsFromToken(final String token) {
+    public String getUsername(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(secretKey)
                 .build()
                 .parseClaimsJws(token)
-                .getBody();
+                .getBody()
+                .getSubject();
     }
 
-    /**
-     * token 검증
-     *
-     * @param token JWT
-     * @return token 검증 결과
-     */
-    public Boolean validateToken(final String token) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (SecurityException e) {
-            log.warn("Invalid JWT signature: {}", e.getMessage());
-        } catch (MalformedJwtException e) {
-            log.warn("Invalid JWT token: {}", e.getMessage());
-        } catch (ExpiredJwtException e) {
-            log.warn("JWT token is expired: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            log.warn("JWT token is unsupported: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            log.warn("JWT claims string is empty: {}", e.getMessage());
-        }
-        return false;
+    public String getEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("email", String.class);
     }
 
 }
