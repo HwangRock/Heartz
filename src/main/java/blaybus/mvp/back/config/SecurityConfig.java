ffffddef0febeaa47e6b9d348d4020d1e1e18e4a
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-02-17 01:59:12 +0900

    feat: security 인증필터

--- src/main/java/blaybus/mvp/back/config/SecurityConfig.java
+++ src/main/java/blaybus/mvp/back/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package blaybus.mvp.back.config;
 
 import blaybus.mvp.back.jwt.TokenProvider;
+import blaybus.mvp.back.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -19,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // 새롭게 변경된 CSRF 비활성화 방식
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/oauth2/authorization/google", "/login/oauth2/code/google", "/error", "/favicon.ico").permitAll()
                         .anyRequest().authenticated()
@@ -27,7 +29,9 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
                 .oauth2Login(oauth -> oauth
                         .defaultSuccessUrl("/login-success", true)
-                );
+                )
+                // JWT 인증 필터 추가
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // JWT 인증 필터 추가
 
         return http.build();
     }
