e6e60d02d473c6a45ff2ae7c6bd67fa70ab4d3ff
choeunbin03 <choeunbiin03@naver.com>
2025-02-17 15:04:09 +0900

    Hackathon-BE-10 [FEAT] 디자이너 스케줄 구현

--- src/main/java/blaybus/mvp/back/controller/ClientController.java
+++ src/main/java/blaybus/mvp/back/controller/ClientController.java
@@ -1,34 +1,47 @@
 package blaybus.mvp.back.controller;
 
-import blaybus.mvp.back.dto.response.LoginResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import blaybus.mvp.back.jwt.TokenProvider;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.UUID;
-
 @RestController
-@RequestMapping(value="")
+@RequestMapping("/api/v1/auth")
 public class ClientController {
 
-    @GetMapping("/login/success")
-    public ResponseEntity<Object> loginSuccess(LoginResponse loginResponse) throws URISyntaxException {
-        URI redirectUri = new URI("http://localhost:3000/login/redirect");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(redirectUri);
-        httpHeaders.add("Authorization", "Bearer " + loginResponse.getAccessToken());
-        return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
+    private final TokenProvider tokenProvider;
+
+    public ClientController(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    // OAuth2 인증 성공 후 호출되는 엔드포인트
+    @GetMapping("/login-success")
+    public String loginSuccess(@AuthenticationPrincipal OAuth2User oauth2User) {
+        String username = oauth2User.getAttribute("name");
+        String email = oauth2User.getAttribute("email");
+
+        // JWT 생성
+        String token = tokenProvider.createToken(username, email);
+
+        // Bearer 토큰 형태로 반환
+        return "Bearer " + token;
+    }
+
+    // 토큰 생성 테스트용 API
+    @GetMapping("/test-token")
+    public String testToken() {
+        // Mock 데이터
+        String username = "test_user";
+        String email = "test_user@example.com";
+
+        // JWT 생성
+        String token = tokenProvider.createToken(username, email);
+
+        // Bearer 토큰 형태로 반환
+        return "Bearer " + token;
     }
 
 }
